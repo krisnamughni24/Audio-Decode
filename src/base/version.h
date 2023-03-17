5e1ebfab5bbb634ca58d5a196c1a479667641629
Krisna Mughni <68163712+krisnamughni24@users.noreply.github.com>
2023-03-17 07:01:06 +0700

    Add files via upload

---
+++ src/base/version.h
@@ -0,0 +1,4 @@
+// This file was automatically created by ./get_version.sh.
+// It is only included by ./kaldi-error.cc.
+#define KALDI_VERSION "5.5.0-f523"
+#define KALDI_GIT_HEAD "f52309c6d9ec6c72c5cc0cbbdb459c9d72ff706d"
